d4a59e6323a205aa64ab8b5d2c7aa62346e09e7c
oleksiy <oleksii.kulyk@knights.ucf.edu>
2020-09-14 09:57:04 -0400

    Added a .json file

---
+++ kulyk_p1/src/Main.java
@@ -0,0 +1,11 @@
+package src;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello World, " + args[0] + "!");
+    }
+    /*void testEncrypt1234to0189() {
+        Encrypter e = new Encrypter();
+        assertEquals("0189", e.encrypt("1234"));
+    }*/
+}
