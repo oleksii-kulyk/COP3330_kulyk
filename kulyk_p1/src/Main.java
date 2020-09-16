cdee5bdb5ef2d60fcc7e6d7b90f111bd9ec2f361
oleksiy <oleksii.kulyk@knights.ucf.edu>
2020-09-15 23:49:41 -0400

    Added input and user choice. Testing.

--- kulyk_p1/src/Main.java
+++ kulyk_p1/src/Main.java
@@ -3,30 +3,54 @@ import java.util.Scanner;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         // Create a Scanner object
         Scanner keyboard = new Scanner(System.in);
-        // Entering a string of four digit integer
+        // User choice
+        System.out.println("To Encrypt press 'e'\nTo Decrypt press 'd'");
+        char choice = keyboard.next().charAt(0);
+        // Reading data
+        System.out.println("Now enter your data");
+        String data = keyboard.nextLine();
 
-    }
+        // Encrypting and Printing
+        if (choice == 'e') {
+            Encrypter e = new Encrypter();
+            e.data = e.encrypt(data);
+            System.out.println(e.data);
+        }
 
+        // Decrypting and Printing
+        if (choice == 'd') {
+            Decrypter d = new Decrypter();
+            d.data = d.decrypt(data);
+            System.out.println(d.data);
+        }
 
+        // Test case from professor
+        /*
         void testEncrypt1234to0189() {
             Encrypter e = new Encrypter();
             assertEquals("0189", e.encrypt("1234"));
+        }
+         */
     }
 }
 
-class Encrypter{
-    public static String encrypt (String data){
+class Encrypter {
+    String data;
+    public static String encrypt(String data) {
         char[] datum = data.toCharArray();
-        String ones = Integer.toString ((datum[3] + 7 - '0') % 10);
-        String tens = Integer.toString ((datum[2] + 7 - '0') % 10);
-        String hunds =Integer.toString ((datum[1] + 7 - '0') % 10);
-        String thouds=Integer.toString ((datum[0] + 7 - '0') % 10);
+        String ones = Integer.toString((datum[3] + 7 - '0') % 10);
+        String tens = Integer.toString((datum[2] + 7 - '0') % 10);
+        String hunds = Integer.toString((datum[1] + 7 - '0') % 10);
+        String thouds = Integer.toString((datum[0] + 7 - '0') % 10);
         return tens + ones + thouds + hunds;
     }
+}
 
+class Decrypter{
+    String data;
     public static String decrypt (String data){
         char[] datum = data.toCharArray();
         String ones = Integer.toString (Math.floorMod((datum[3] - 7 - '0'), 10));
