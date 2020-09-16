94798715d9ed4b0b8815d8795bc75b3ab8c7cb16
oleksiy <oleksii.kulyk@knights.ucf.edu>
2020-09-15 22:15:30 -0400

    Working Encrypter.encrypt method

--- kulyk_p1/src/Main.java
+++ kulyk_p1/src/Main.java
@@ -1,6 +1,6 @@
 package src;
 import java.util.Scanner;
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Main {
     public static void main(String[] args){
@@ -20,19 +20,10 @@ public class Main {
 class Encrypter{
     public static String encrypt (String data){
         int datum = Integer.parseInt(data);
-        int ones = (datum % 10 + 7) % 10;
-        int tens = ((datum / 10) % 10 + 7) % 10;
-        int hunds = ((datum / 100) % 10 + 7) % 10;
-        int thouds = ((datum / 1000) % 10 + 7) % 10;
-        //swapping 1 and 3
-        int temp = ones;
-        ones = hunds;
-        hunds = temp;
-        //sawpping 2 and 4
-        temp  = tens;
-        tens = thouds;
-        thouds = temp;
-        datum = thouds*1000 + hunds*100 + tens*10 + ones;
-        return data = Integer.toString(datum);
+        String ones = Integer.toString ((datum % 10 + 7) % 10);
+        String tens = Integer.toString (((datum / 10) % 10 + 7) % 10);
+        String hunds =Integer.toString (((datum / 100) % 10 + 7) % 10);
+        String thouds=Integer.toString (((datum / 1000) % 10 + 7) % 10);
+        return tens + ones + thouds + hunds;
     }
 }
